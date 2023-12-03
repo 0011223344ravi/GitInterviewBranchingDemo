71de8eeed4fd1d902a9d3d126b9b119b93500b2e
Ravi Kumar Gupta <0011223344ravi@gmail.com>
2023-12-03 09:33:09 +0530

    first commit

---
+++ src/main/java/com/example/gitinterviewquestionsbranchdemo/GitInterviewQuestionsBranchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitinterviewquestionsbranchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitInterviewQuestionsBranchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitInterviewQuestionsBranchDemoApplication.class, args);
+    }
+
+}
